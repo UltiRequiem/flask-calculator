67c16e3484960ac66ad57ebf0bf3f5edcb542f5d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-04-04 18:47:31 -0700

    🎨 Improve format

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import render_template, request
 from keep_alive import keep_alive, app
 
 @app.route('/')
--- keep_alive.py
+++ keep_alive.py
@@ -2,9 +2,7 @@ from flask import Flask
 from threading import Thread
 import random
 
-
 app = Flask('')
-
 
 def run():
   app.run(host='0.0.0.0',port=random.randint(2000,9000))
