5ca24cb5753a4ec4018ccaf63161805e886fd9ea
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-14 15:20:46 -0500

    :art: Format Code

--- keep_alive.py
+++ keep_alive.py
@@ -2,11 +2,11 @@ from flask import Flask
 from threading import Thread
 import random
 
-app = Flask('')
+app = Flask("")
 
 
 def run():
-    app.run(host='0.0.0.0', port=random.randint(2000, 9000))
+    app.run(host="0.0.0.0", port=random.randint(2000, 9000))
 
 
 def keep_alive():
