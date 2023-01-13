25caa5ff303737293c8e309ce9ff0fd528de3351
Sabinkari Rahul <sabinkari.rahul@expleogroup.com>
2023-01-13 14:17:46 +0530

    operations changed

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Devprojects\flask calculator\flask-calculator\venv
+set VIRTUAL_ENV=C:\Users\srahul\Documents\flask-calculator\flask-calculator\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
